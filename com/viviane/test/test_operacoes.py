dc80959ecaf08f4f5722ce7c0bfa91b93786721c
medeirosvivianne <viviane.teixeira@aluno.faculdadeimpacta.com.br>
2020-10-08 01:21:17 -0300

    modificacao do arquivo test_operacoes.py e upload da pasta prints

--- com/viviane/test/test_operacoes.py
+++ com/viviane/test/test_operacoes.py
@@ -1,8 +1,8 @@
-def  fun(x):  
-  return x + 3  
-def teste_positivo():  
-  assert fun(10) == 13  
-def teste_negativo():  
-  assert fun(-7) == -4  
-def teste_zero():  
-  assert fun (0) == 3  
+def  fun(x):
+  return x + 3
+def teste_positivo():
+  assert fun(10) == 13
+def teste_negativo():
+  assert fun(-7) == -4
+def teste_zero():
+  assert fun (0) == 3
